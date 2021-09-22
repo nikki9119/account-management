8e943c9fdc67f3239a538bfe427679a720158ef7
Nikhil Kumar <a.p.nikhilkumar@gmail.com>
2021-09-22 16:23:20 +0530

    Some debugging

--- src/main/java/com/bank/accountmanagement/Repositories/AccountRepository.java
+++ src/main/java/com/bank/accountmanagement/Repositories/AccountRepository.java
@@ -4,15 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bank.accountmanagement.Models.Account;
-import com.bank.accountmanagement.Models.Transaction;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
 	
 	Optional<Account> findById(String accountNumber);
+	Optional<List<Account>> findByCustomerCustomerId(int customerId);
 	
 }
